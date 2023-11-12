04a340c5bb1268eaaf2207717d3d2dad4665dc1b
KatsuMouley <130433332+KatsuMouley@users.noreply.github.com>
2023-11-11 23:05:36 -0300

    adding JS file

---
+++ JS/variables.cpp
@@ -0,0 +1,7 @@
+#include <sdh.io>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ JS/variables.js
@@ -0,0 +1,31 @@
+let int = 25;
+let nome = "João";
+let message = 'Bem-vindo!';
+let tValue = true;
+let fValue = false;
+let float = 2.5;
+let arrayA = [1, 2, 3, 4, 5];
+let valorNull = null;
+let undefined;
+let matrixA = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+
+let x = 5;
+let y = 10;
+let resultado = x + y;
+let isEqual = x === y;
+let isGreater = x > y;
+
+let pessoaObj = {
+    nome: "Maria",
+    idade: 30,
+    cidade: "São Paulo"
+};
+
+
+console.log(arrayA[0]);
+console.log(matrixA[0][2]);
