6f1441ae351c70f275dd2cdfd805e0b7d983b8f1
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-01-02 12:09:50 -0300

    .

---
+++ class1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int userI1, userI2, PROD;
+    std::cin >> userI1;
+    std::cin >> userI2;
+    PROD = userI1 * userI2;
+    std::cout << "PROD = " << PROD << std::endl;
+    return 0;
+}
