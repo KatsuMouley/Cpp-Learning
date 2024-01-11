e54e381552e16ba75a57688e026eee05343b7da3
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-01-11 12:01:02 -0300

    .

--- class2.cpp
+++ class2.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <iomanip>
 // avarage calculus
 
 int main()
 {
-    int a;
-    std::cin >> a;
-    std::cout << a << std::endl;
+    double a, b, avarage;
+    std::cin >> a >> b;
+    avarage = (a * 3.5 + b * 7.5) / (3.5 + 7.5);
+    std::cout << std::fixed << std::setprecision(5) << "MEDIA = " << avarage << std::endl;
     return 0;
 }
