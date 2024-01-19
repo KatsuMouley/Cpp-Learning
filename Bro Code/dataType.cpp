81a868975316d37f3ad090ea2a7ce2037bec1de0
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-01-19 11:57:23 -0300

    .

--- Bro Code/dataType.cpp
+++ Bro Code/dataType.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+void dataTypes();
+
 int main()
 {
-
+    dataTypes();
     return 0;
+}
+
+void dataTypes()
+{
+    std::cout << "there are some types of datas" << std::endl;
+    int wholeNumbers = 91;
+    double decimalNumbers1 = 9.9;
+    float decimalNumbers2 = 9.99;
+    char c = 'X', array[9] = "array123", strings[] = "THIS IS A STRING";
+    // Arrays start from 0
+    // strings are arrays for words
+    // char alone is for single characters
+    std::cout << wholeNumbers << "| Integers"
+              << "\n"
+              << decimalNumbers1 << "| Doubles"
+              << "\n"
+              << decimalNumbers2 << "| Floats"
+              << "\n"
+              << c << "| single characters"
+              << "\n"
+              << array << "| Arrays"
+              << "\n"
+              << strings << "| Strings" << std::endl;
 }
