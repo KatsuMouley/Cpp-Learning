e87627638eabc78a680e560ad537e60c38382b5d
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-01-22 12:32:15 -0300

    ..

---
+++ Bro Code/typedef.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+/*
+typedef std::string text_t;
+typedef int number_t;
+*/
+// or we  could use
+using text_t = std::string;
+using number_t = int;
+
+int main()
+{
+    text_t string = "Hello World!";
+
+    number_t number = 35;
+
+    std::cout << string << "\n";
+    std::cout << number << "\n";
+    return 0;
+}
+
+/*
+    typedef = reserved key word used to create an aditional name (alias) for another data type;
+*/
