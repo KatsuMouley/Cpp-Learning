68cf120d5c43427b0f76ea0964f966c932d30278
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-01-22 11:46:59 -0300

    .

--- class3.cpp
+++ class3.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-
+    double A, B, C, avarage;
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    scanf("%lf", &C);
+    avarage = (((A * 2) + (B * 3) + (C * 5)) / 10);
+    printf("MEDIA = %.2lf\n", avarage);
     return 0;
 }
