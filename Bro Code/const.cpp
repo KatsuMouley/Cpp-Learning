d7d37da04d5570631c7c5799648282bed1392493
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-01-22 11:05:54 -0300

    .

---
+++ Bro Code/const.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    const double PI = 3.14;
+    int raio, distance;
+    std::cout << "Please input the ray of the circuference" << std::endl;
+
+    return 0;
+}
--- Bro Code/dataType.cpp
+++ Bro Code/dataType.cpp
@@ -15,18 +15,23 @@ void dataTypes()
     double decimalNumbers1 = 9.9;
     float decimalNumbers2 = 9.99;
     char c = 'X', array[9] = "array123", strings[] = "THIS IS A STRING";
+    // Other way to do strings in C++ is using std::string
+    std::string stdString = "Katsu Mouley";
     // Arrays start from 0
     // strings are arrays for words
     // char alone is for single characters
-    std::cout << wholeNumbers << "| Integers"
-              << "\n"
-              << decimalNumbers1 << "| Doubles"
-              << "\n"
-              << decimalNumbers2 << "| Floats"
-              << "\n"
-              << c << "| single characters"
-              << "\n"
-              << array << "| Arrays"
-              << "\n"
-              << strings << "| Strings" << std::endl;
+    std::cout
+        << wholeNumbers << "| Integers"
+        << "\n"
+        << decimalNumbers1 << "| Doubles"
+        << "\n"
+        << decimalNumbers2 << "| Floats"
+        << "\n"
+        << c << "| single characters"
+        << "\n"
+        << array << "| Arrays"
+        << "\n"
+        << strings << "| strings with char[]"
+        << "\n"
+        << stdString << "| strings with std::string" << std::endl;
 }
