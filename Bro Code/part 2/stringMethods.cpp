3b047cba409f5fdfeaca415352a5ac6dcabf37e3
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-02-08 05:48:06 -0300

    .

--- Bro Code/part 2/stringMethods.cpp
+++ Bro Code/part 2/stringMethods.cpp
@@ -6,4 +6,14 @@ int main()
 
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
+
+    if (name.length() > 12)
+    {
+        std::cout << "Your name is too big!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Welcome! " << std::endl;
+    }
+    std::cout << name;
 }
