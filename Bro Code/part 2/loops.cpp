4b9af18d026a039a8f8452354952ff99d39a3c93
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-02-22 12:32:46 -0300

    .

--- Bro Code/part 2/loops.cpp
+++ Bro Code/part 2/loops.cpp
@@ -1,10 +1,15 @@
+#include <string>
 #include <iostream>
 
 int main()
 {
     std::string name;
+    while (name.empty())
+    {
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
 
-    while
-
+    std::cout << "Hello " << name << std::endl;
     return 0;
 }
