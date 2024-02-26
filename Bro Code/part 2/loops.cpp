c8d74268496cf06bda1f752fc056f451693f4468
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-02-25 23:28:15 -0300

    .

--- Bro Code/part 2/loops.cpp
+++ Bro Code/part 2/loops.cpp
@@ -16,11 +16,17 @@ int main()
     std::cout << "But it may take time to create something actually good since my progress isn't fastest like others" << std::endl;
     std::string condoriano;
     std::cout << "Who's better?\nSanji\nZoro\nPlease be aware that there's only on option(Condoriano)\n";
+
     do
     {
         std::cout << "Your answer: ";
         std::getline(std::cin, condoriano);
     } while (condoriano != "Condoriano");
+
+    for (int i = 0; i <= 10; i++)
+    {
+        std::cout << "SKBIDI TOILET " << i << std::endl;
+    }
 
     return 0;
 }
