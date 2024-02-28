6e4feb2d44caceef3e4a47e0b8231c84b2d712a0
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-02-28 08:37:36 -0300

    .

--- Bro Code/part 2/nestedloop.cpp
+++ Bro Code/part 2/nestedloop.cpp
@@ -19,7 +19,7 @@ int main()
     {
         for (int j = 1; j <= c; j++)
         {
-            if (i == x && j == y)
+            if (i == y && j == x)
             {
                 std::cout << "[X]";
             }
@@ -30,4 +30,6 @@ int main()
         }
         std::cout << std::endl;
     }
+
+    return 0;
 }
