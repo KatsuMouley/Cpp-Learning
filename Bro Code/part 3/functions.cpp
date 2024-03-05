19172c7113a5090dbc1e8d4214c461450338fce1
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-05 07:18:23 -0300

    .

---
+++ Bro Code/part 3/functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void function(std::string name);
+
+int main()
+{
+    std::string name;
+
+    std::cout << "Insert your name: ";
+    std::cin << name;
+    function(name);
+
+    // An function capable of finding the points of collision between two objects
+}
+
+void function(std::string name)
+{
+    std::cout << "Hello " << name << std::endl;
+}
