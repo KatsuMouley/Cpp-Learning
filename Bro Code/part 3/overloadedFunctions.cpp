b8686bd6f4005660f1b4b71f70fbed37af141c74
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-10 23:50:24 -0300

    Overloaded functions

--- Bro Code/part 3/overloadedFunctions.cpp
+++ Bro Code/part 3/overloadedFunctions.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+void bakeCake();
+void bakeCake(std::string topping1);
+void bakeCake(std::string topping1, std::string topping2);
+
 int main(){
+    bakeCake();
+    bakeCake("Chocolate");
+    bakeCake("Chocolate","Strawberries");
     return 0;
+}
+
+void bakeCake(){
+    std::cout << "Here's your cake!\n";
+}
+void bakeCake(std::string topping1){
+    std::cout << "Here's your "<< topping1<<" cake!\n";
+}
+void bakeCake(std::string topping1, std::string topping2){
+    std::cout << "Here's your with "<< topping1 <<" cake with "<< topping2<<" topping!\n";
 }
