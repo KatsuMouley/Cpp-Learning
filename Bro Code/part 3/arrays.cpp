4267b7b3bef173b4e9575ab34a2642e03e62107c
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-20 12:31:27 -0300

    Size of an array

--- Bro Code/part 3/arrays.cpp
+++ Bro Code/part 3/arrays.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+using std::string, std::cout, std::cin;
+
+int main(){
+    string words[] = {"SPEED","FAMILY","POOR","RICH"};
+    cout << words[0] << "\n------\n";
+    int sizeOfArray = (sizeof(words)/sizeof(*words));
+    for (int i = 0; i < sizeOfArray; i++){
+        cout << words[i] << "\n";
+    }
+
+    
+    return 0;
+}
