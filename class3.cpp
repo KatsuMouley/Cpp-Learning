59943fc844acc373a759b977e9f2efa192184458
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-04-04 19:16:17 -0300

    Hello World

--- class3.cpp
+++ class3.cpp
@@ -8,5 +8,6 @@ int main()
     scanf("%lf", &C);
     avarage = (((A * 2) + (B * 3) + (C * 5)) / 10);
     printf("MEDIA = %.2lf\n", avarage);
+    printf("Hello world! %0f", 1.65);
     return 0;
 }
