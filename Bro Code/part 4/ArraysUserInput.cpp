64e2a68eca3c9e42fda41cd054a33f558a10eba4
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-05-01 23:06:00 -0300

    arrays final

---
+++ Bro Code/part 4/ArraysUserInput.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string foods[5], temp;
+    int size = sizeof(foods)/sizeof(foods[0]);
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter a food you like or q to quit #" << i << ": ";
+        std::getline(std::cin, temp);
+        if (temp == "q" || temp == "Q")
+        {
+            break;
+        } else{
+            foods[i] = temp;
+        }
+        
+    }
+
+    std::cout << "You like the following food:\n";
+
+    // for (std::string food : foods)
+    // {
+    //     std::cout << food << '\n';
+    // }
+    
+    for (int i = 0; !foods[i].empty(); i++)
+    {
+        std::cout << foods[i] << '\n';
+    }
+    
+    
+    return 0;
+}
